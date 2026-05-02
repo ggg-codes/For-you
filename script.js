3a6c65f384756e12010cb793603cbb891742138a
ghali <ghali.belmkaddam@gmail.com>
2026-05-02 03:07:51 +0100

    finally done

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(SplitText);
+});
+
 const ahBtn = document.getElementById("ah");
 const laBtn = document.getElementById("la");
 const questionText = [
@@ -15,8 +19,8 @@ const speechBubbleText = [
 ];
 const question = document.querySelector(".question");
 const speechbBubble = document.querySelector(".speech-bubble");
-
-// speechbBubble.innerText = speechBubbleText[0];
+const gif = document.getElementById("gif");
+const btnContainer = document.querySelector(".btn-container");
 
 let i = 0;
 let x = 0;
@@ -32,7 +36,6 @@ laBtn.addEventListener("click", () => {
     fontSize: parseFloat(fontSize) * scale + "px",
   };
 
-  // console.log(newRect.fontSize);
   gsap.to(ahBtn, {
     duration: 0.5,
     width: newRect.width,
@@ -58,4 +61,50 @@ laBtn.addEventListener("click", () => {
     x = 0;
     speechbBubble.innerText = speechBubbleText[x];
   }
+});
+
+ahBtn.addEventListener("click", () => {
+  speechbBubble.style.display = "none";
+  gif.src = "media/final.gif";
+  question.innerText = "AAAA !!! KNT 3ARFK KATBGHINI";
+  btnContainer.innerHTML = '<h1 class="hearts">❤️🩷💕💞💓💗💗</h1>';
+  const hearts = document.querySelector(".hearts");
+
+  let split = SplitText.create(hearts, { type: "chars" });
+  gsap.from(split.chars, {
+    y: 100,
+    autoAlpha: 0,
+    stagger: {
+      each: 0.1,
+    },
+    ease: "back.out(2.5)",
+  });
+
+  const defaults = {
+    spread: 360,
+    ticks: 100,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    shapes: ["heart"],
+    colors: ["#f95e9b"],
+  };
+
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
 });
